9e20cc9469cac0dfc9e686604b23ed4bda1dc3e7
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-25 22:39:17 +0100

    make multistep default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { z } from "zod";
-import { Controller } from "react-hook-form";
+
 import { Input } from "~/ui/input";
 import { Button } from "~/ui/button";
 import { Label } from "~/ui/label";
@@ -14,92 +14,152 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/ui/select";
-import Link from "next/link";
 import { useZodForm } from "~/utils/zod-form";
+import { Controller } from "react-hook-form";
 
-const PET = {
-  DOG: "DOG",
-  CAT: "CAT",
-  BIRD: "BIRD",
+const countries = {
+  US: "US",
+  UK: "UK",
+  SE: "SE",
 } as const;
 
-const schema = z.object({
-  name: z.string().min(3).max(20),
-  email: z.string().email(),
-  text: z.string().min(50),
-  pet: z.nativeEnum(PET),
-});
+const steps = {
+  1: z.object({
+    name: z.string().min(3).max(20),
+    email: z.string().email(),
+  }),
+  2: z.object({
+    country: z.nativeEnum(countries),
+    postalCode: z.string().min(3).max(20),
+  }),
+  3: z.object({
+    text: z.string(),
+  }),
+};
+type FormData = z.infer<typeof steps[keyof typeof steps]>;
 
-export default function FormTest() {
-  const methods = useZodForm({
-    schema,
-    defaultValues: {
-      name: "",
-      email: "",
-      text: "",
-      pet: "DOG",
-    },
-  });
-
-  const [result, setResult] = useState({ name: "", email: "" });
-  const onSubmit = methods.handleSubmit(
-    (data) => {
-      setResult(data); // send to backend or smth
-    },
-    (e) => {
-      console.log(e);
-    }
-  );
+export default function Multistep() {
+  const [step, setStep] = useState(1);
+  const [composedData, setComposedData] = useState<FormData>();
 
   return (
     <div className="max-w-2xl py-16 mx-auto space-y-8">
-      <Link href="/multistep">Go to multistep form</Link>
-      <form action="" className="flex flex-col gap-4" onSubmit={onSubmit}>
-        <div className="space-y-1">
+      <h1 className="text-3xl font-bold">Multistep form</h1>
+      <div className="space-y-8">
+        {step === 1 && (
+          <Step1
+            onStepDone={(data) => {
+              setStep(2);
+              setComposedData((prev) => ({ ...prev, ...data }));
+            }}
+          />
+        )}
+        {step === 2 && (
+          <Step2
+            onStepDone={(data) => {
+              setStep(3);
+              setComposedData((prev) => ({ ...prev, ...data }));
+            }}
+          />
+        )}
+        {step === 3 && (
+          <Step3
+            onStepDone={(data) => {
+              setStep(4);
+              setComposedData((prev) => ({ ...prev, ...data }));
+            }}
+          />
+        )}
+        {step === 4 && (
+          <Result
+            data={composedData}
+            restart={() => {
+              setStep(1);
+              setComposedData(undefined);
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Step1(props: {
+  onStepDone: (data: z.infer<typeof steps[1]>) => void;
+}) {
+  const methods = useZodForm({
+    schema: steps[1],
+  });
+
+  return (
+    <div className="space-y-2">
+      <form
+        className="space-y-4"
+        onSubmit={methods.handleSubmit((data) => {
+          console.log("Step 1", data);
+          props.onStepDone(data);
+        })}
+      >
+        <div>
           <Label htmlFor="name">Name</Label>
           <Input
             id="name"
-            className="bg-slate-800"
+            placeholder="Name"
             {...methods.register("name")}
+            className="w-full"
           />
           <p className="text-red-500 font-medium">
             {methods.formState.errors?.name?.message}
           </p>
         </div>
-        <div className="space-y-1">
+
+        <div>
           <Label htmlFor="email">Email</Label>
           <Input
             id="email"
-            className="bg-slate-800"
+            placeholder="Email"
             {...methods.register("email")}
+            className="w-full"
           />
           <p className="text-red-500 font-medium">
             {methods.formState.errors?.email?.message}
           </p>
         </div>
-        <div className="space-y-1">
-          <Label htmlFor="text">Text</Label>
-          <Textarea
-            id="text"
-            className="bg-slate-800"
-            {...methods.register("text")}
-          />
-          <p className="text-red-500 font-medium">
-            {methods.formState.errors?.text?.message}
-          </p>
-        </div>
-        <div className="space-y-1">
-          <Label htmlFor="pet">Pet</Label>
+
+        <Button type="submit">Next</Button>
+      </form>
+    </div>
+  );
+}
+
+function Step2(props: {
+  onStepDone: (data: z.infer<typeof steps[2]>) => void;
+}) {
+  const methods = useZodForm({
+    schema: steps[2],
+  });
+
+  return (
+    <div className="space-y-1">
+      <form
+        className="space-y-4"
+        onSubmit={methods.handleSubmit((data) => {
+          console.log("Step 2", data);
+          props.onStepDone(data);
+        })}
+      >
+        <div>
+          <Label htmlFor="country">Country</Label>
           <Controller
             control={methods.control}
-            name="pet"
+            name="country"
             render={({ field }) => (
               <Select value={field.value} onValueChange={field.onChange}>
-                <SelectTrigger className="bg-slate-800">
-                  <SelectValue placeholder="Select a fruit" />
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Where are you from?" />
                 </SelectTrigger>
                 <SelectContent>
-                  {Object.entries(PET).map(([key, value]) => (
+                  {Object.entries(countries).map(([key, value]) => (
                     <SelectItem key={key} value={value}>
                       {value}
                     </SelectItem>
@@ -110,14 +170,69 @@ export default function FormTest() {
           />
 
           <p className="text-red-500 font-medium">
-            {methods.formState.errors?.pet?.message}
+            {methods.formState.errors?.country?.message}
+          </p>
+        </div>
+
+        <div>
+          <Label htmlFor="postalCode">Postal code</Label>
+          <Input
+            id="postalCode"
+            placeholder="Postal code"
+            {...methods.register("postalCode")}
+            className="w-full"
+          />
+          <p className="text-red-500 font-medium">
+            {methods.formState.errors?.postalCode?.message}
+          </p>
+        </div>
+
+        <Button type="submit">Next</Button>
+      </form>
+    </div>
+  );
+}
+
+function Step3(props: {
+  onStepDone: (data: z.infer<typeof steps[3]>) => void;
+}) {
+  const methods = useZodForm({
+    schema: steps[3],
+  });
+
+  return (
+    <div className="space-y-1">
+      <form
+        className="space-y-4"
+        onSubmit={methods.handleSubmit((data) => {
+          console.log("Step 3", data);
+          props.onStepDone(data);
+        })}
+      >
+        <div>
+          <Label htmlFor="text">Additional information</Label>
+          <Textarea
+            id="text"
+            placeholder="Text"
+            {...methods.register("text")}
+            className="w-full"
+          />
+          <p className="text-red-500 font-medium">
+            {methods.formState.errors?.text?.message}
           </p>
         </div>
         <Button type="submit">Submit</Button>
       </form>
+    </div>
+  );
+}
 
-      <pre>Watch: {JSON.stringify(methods.watch(), null, 2)}</pre>
-      <pre>Result: {JSON.stringify(result, null, 2)}</pre>
+function Result(props: { data?: FormData; restart: () => void }) {
+  return (
+    <div className="space-y-1">
+      <h2 className="text-xl font-bold">Result</h2>
+      <pre>{JSON.stringify(props.data, null, 2)}</pre>
+      <Button onClick={() => props.restart()}>Restart</Button>
     </div>
   );
 }
